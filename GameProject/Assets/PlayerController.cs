1f9e33dc10020e55558f5bf3e7126c55b8ddde45
Midhat12 <sp18-bse-097@isbstudent.comsats.edu.pk>
2020-06-09 15:43:16 +0500

    project
    project

---
+++ GameProject/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float Speed;
+
+    private Rigidbody rbi;
+
+   
+
+    void Start () {
+        rbi = GetComponent<Rigidbody>();
+	}
+
+	void Update ()
+    {
+        float HorizontalMovment = Input.GetAxis("Horizontal");
+        float VerticalMovment = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rbi.AddForce(move * Speed);
+    }
+
+   
+}
